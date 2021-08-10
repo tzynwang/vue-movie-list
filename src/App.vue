b597f342e47b09080c25c2047e04f0c1a75855da
tzyn (windows) <tzyn.wang@gmail.com>
2021-08-10 17:30:23 +0800

    feat: set project App.vue and main.js

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <headerNav />
+    <main class="container">
+      <router-view/>
+    </main>
+  </div>
+</template>
+
+<script>
+import 'bootstrap' // bootstrap.min.js
+import "bootstrap-icons/font/bootstrap-icons.css"
+import "@/assets/css/bootstrap.darkly.css";
+import "@/assets/css/style.css";
+
+import headerNav from './components/headerNav'
+
+export default {
+  name: 'App',
+  components: {
+    headerNav
+  }
+}
+</script>
